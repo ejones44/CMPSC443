d84fd9d157aa2b3e9a1cc7f424fea475b8d7475d
Eddy Jones <ejones44@gmail.com>
2016-05-03 21:52:12 -0400

    Added files via upload

--- detector.py
+++ detector.py
@@ -1,7 +1,10 @@
 import os
 import glob
 
-files_read = 0
+obs_files_read = 0
+unobs_files_read = 0
+false_positives = 0
+missed_detections = 0
 detections = 0
 vowels = list("aeiouyAEIOUY")
 consonants = list("bcdfghjklmnpqrstvxzBCDFGHJKLMNPQRSTVXZ")
@@ -12,8 +15,8 @@ for filename in glob.glob('*.js'):
     #open file
     f = open(filename, 'r')
     #variable init
-    detections_thisFile = 0
-    linesRead_thisFile = 0
+    detections_this_file = 0
+    lines_read_this_file = 0
 
     #top line of file is file name (strip first two chars as they are '//')
     file_top_line = f.readline()[2:-1]
@@ -21,109 +24,120 @@ for filename in glob.glob('*.js'):
 
     #next line of file is obfuscator name (strip first two chars as they are '//')
     obfuscator_name = f.readline()[2:-1]
+
     print "Obfuscator - " + obfuscator_name
-    print "================="
+    #print "================="
     for line in f:
-        linesRead_thisFile += 1
+        lines_read_this_file += 1
         lowered = line.lower()
         if "document.write" in lowered:
-            detections_thisFile += 1
-            print "Found document.write"
+            detections_this_file += 1
+            #print "Found document.write"
         if "eval(" in lowered:
-            detections_thisFile += 1
-            print "Found eval()"
+            detections_this_file += 1
+            #print "Found eval()"
         if "p,a,c,k,e,d" in lowered:
-            detections_thisFile += 1
-            print "Found p,a,c,k,e,d"
+            detections_this_file += 1
+            #print "Found p,a,c,k,e,d"
         if "p,a,c,k,e,r" in lowered:
-            detections_thisFile += 1
-            print "Found p,a,c,k,e,r"
+            detections_this_file += 1
+            #print "Found p,a,c,k,e,r"
         #unescape
         if "unescape" in lowered:
-            detections_thisFile += 1
-            print "Found unescape"
+            detections_this_file += 1
+            #print "Found unescape"
         #fromCharCode
         if "fromcharcode" in lowered:
-            detections_thisFile += 1
-            print "Found fromCharCode"
+            detections_this_file += 1
+            #print "Found fromCharCode"
+
         #analyzing function names
-        try :
-            functionLocation =  line.index('function')
-        except ValueError:
-            pass
-        else:
-            if functionLocation > -1:
-                functionNameEnd = line.find('(',functionLocation)
-                functionName = line[functionLocation:functionNameEnd]
-                #some functions are anonymous and dont have names
-                if len(functionName) > 0:
-                    #count consonants
-                    num_cons = sum(functionName.count(c) for c in consonants)
-                    #count vowels
-                    num_vwls = sum(functionName.count(c) for c in vowels)
-                    #count special chars
-                    num_others = sum(functionName.count(c) for c in others)
-                    print "Found a function with %d consonants, %d vowels and %d special or numeric characters" % (num_cons, num_vwls, num_others)
-                    if (num_cons/num_vwls) > 3:
-                        print "High consonants/vowels ratio - Likely obfuscated"
-                        detections_thisFile += 1
-                    if num_others > 4:
-                        print "High use of numbers or special characters - Likely obfuscated"
-                        detections_thisFile += 1
-                    if len(functionName) > 25:
-                        print "Function name is very long - Likely obfuscated"
-                        detections_thisFile += 1
+        function_location =  0
+        function_location = line.find('function', function_location)
+        while function_location > -1:
+            function_location += 9
+            function_name_end = line.find('(',function_location)
+            function_name = line[function_location:function_name_end]
+            #some functions are anonymous and dont have names
+            if len(function_name) > 0:
+                #count consonants
+                num_cons = sum(function_name.count(c) for c in consonants)
+                #count vowels
+                num_vwls = sum(function_name.count(c) for c in vowels)
+                #count special chars
+                num_others = sum(function_name.count(c) for c in others)
+                #print "Found a function with %d consonants, %d vowels and %d special or numeric characters" % (num_cons, num_vwls, num_others)
+                if num_vwls == 0 or (num_cons/num_vwls) > 3:
+                    #print "High consonants/vowels ratio - Likely obfuscated"
+                    detections_this_file += 1
+                if num_others > 4:
+                    #print "High use of numbers or special characters - Likely obfuscated"
+                    detections_this_file += 1
+                if len(function_name) > 25:
+                    #print "Function name is very long - Likely obfuscated"
+                    detections_this_file += 1
+            function_location = line.find('function', function_location)
 
         #analyzing variablenames
-        try :
-            varLocation =  line.index('var')
-        except ValueError:
-            pass
-        else:
-            if varLocation > -1:
-                varLocation += 4
-                spaceAfterVar = line.find(' ',varLocation)
-                semicolonAfterVar = line.find(';', varLocation)
-                equalsAfterVar = line.find('=', varLocation)
-                commaAfterVar = line.find(',', varLocation)
-                locationList = [spaceAfterVar, semicolonAfterVar, equalsAfterVar, commaAfterVar]
-                for loc in locationList:
-                    if loc < 0:
-                        locationList.remove(loc)
-                varNameEnd = min(locationList)
-                varName = line[varLocation:varNameEnd]
-                #some vars are anonymous and dont have names
-                if len(varName) > 1:
-                    print "found a var - %s" % varName
-                    #count consonants
-                    num_cons = sum(varName.count(c) for c in consonants)
-                    #count vowels
-                    num_vwls = sum(varName.count(c) for c in vowels)
-                    #count special chars
-                    num_others = sum(varName.count(c) for c in others)
-                    print "Found a var with %d consonants, %d vowels and %d special or numeric characters" % (num_cons, num_vwls, num_others)
-                    if num_vwls == 0 or (num_cons/num_vwls) > 3:
-                        print "High consonants/vowels ratio - Likely obfuscated"
-                        detections_thisFile += 1
-                    if num_others > 4:
-                        print "High use of numbers or special characters - Likely obfuscated"
-                        detections_thisFile += 1
-                    if len(varName) > 25:
-                        print "Var name is very long - Likely obfuscated"
-                        detections_thisFile += 1
+        var_location = 0
+        var_location =  line.find('var', var_location)
+        while var_location > -1:
+            var_location += 4
+            space_after_var = line.find(' ',var_location)
+            semicolon_after_var = line.find(';', var_location)
+            equals_after_var = line.find('=', var_location)
+            comma_after_var = line.find(',', var_location)
+            locationList = [space_after_var, semicolon_after_var, equals_after_var, comma_after_var]
+            for loc in locationList:
+                if loc < 0:
+                    locationList.remove(loc)
+            var_name_end = min(locationList)
+            var_name = line[var_location:var_name_end]
+            #some vars are anonymous and dont have names
+            if len(var_name) > 1:
+                #print "found a var - %s" % var_name
+                #count consonants
+                num_cons = sum(var_name.count(c) for c in consonants)
+                #count vowels
+                num_vwls = sum(var_name.count(c) for c in vowels)
+                #count special chars
+                num_others = sum(var_name.count(c) for c in others)
+                #print "Found a var with %d consonants, %d vowels and %d special or numeric characters" % (num_cons, num_vwls, num_others)
+                if num_vwls == 0 or (num_cons/num_vwls) > 3:
+                    #print "High consonants/vowels ratio - Likely obfuscated"
+                    detections_this_file += 1
+                if num_others > 4:
+                    #print "High use of numbers or special characters - Likely obfuscated"
+                    detections_this_file += 1
+                if len(var_name) > 25:
+                    #print "Var name is very long - Likely obfuscated"
+                    detections_this_file += 1
+            var_location =  line.find('var', var_location)
 
-    # if detected:
-    #     detections = detections + 1
+
     print "=============="
-    print "DETECTIONS - %d" % detections_thisFile
-    print "Lines Read - %d" % linesRead_thisFile
-    detection_score = detections_thisFile/float(linesRead_thisFile)
+    print "DETECTIONS - %d" % detections_this_file
+    print "Lines Read - %d" % lines_read_this_file
+    detection_score = detections_this_file/float(lines_read_this_file)
     print "Score: %f" % detection_score
-    if detection_score > 0.2:
-        detections += 1
-        print "I think this file is obfuscated!"
-    files_read = files_read + 1
+
+
+
+    if "none" in obfuscator_name:
+        unobs_files_read += 1
+        if detection_score > 0.2:
+            false_positives += 1
+    else:
+        obs_files_read += 1
+        if detection_score > 0.2:
+            print "I think this file is obfuscated!"
+            detections += 1
+        else:
+            missed_detections += 1
     print " "
-print "Files read: %d" % files_read
+print "Obfuscated files read: %d" % obs_files_read
+print "Unobfuscated files read: %d" % unobs_files_read
 print "Files detected: %d" % detections
-print "Detection percentage: %f%%" % ((detections/float(files_read))*100)
+print "Detection percentage: %f%%" % ((detections/float(obs_files_read))*100)
+print "False positives: %d" % false_positives
+print "Missed detections %d" % missed_detections
